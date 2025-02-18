3206f8e6b42481be22df972ca05219a0b358e8f5
Youssef Sghairi <sghairiyoussef10@gmail.com>
2025-02-18 17:46:24 +0100

    rmse

--- Evaluation/Accuracy.py
+++ Evaluation/Accuracy.py
@@ -4,3 +4,8 @@ def accuracy_score(y_true, y_pred):
     if len(y_true) != len(y_pred):
         raise ValueError("y_true and y_pred must have the same length")
     return np.mean(np.array(y_true) == np.array(y_pred))
+
+def rmse(y_true, y_pred):
+    y_true, y_pred = np.array(y_true), np.array(y_pred)
+    mse = np.mean((y_true - y_pred) ** 2)
+    return round(np.sqrt(mse), 3)
